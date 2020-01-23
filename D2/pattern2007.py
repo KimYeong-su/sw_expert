33946beead6a5640963bc1450ed9b92d5ec11817
KimYeong-su <suwon0521@naver.com>
2020-01-23 15:53:21 +0900

    January 23rd

--- D2/pattern2007.py
+++ D2/pattern2007.py
@@ -1,10 +1,20 @@
+# 너무 어렵게 짰는데.. while 대신에 for 문으로 짜면 더 간단함
 a = int(input())
 
 for i in range(a):
     b = list(input())
-    c = set(b)
     cnt = 0
-    for k in c:
-        if b.count(k):
-            cnt += 1
-    print(cnt)
+    k=1
+    while k <30:
+        if k>15:
+            if b[0:30-k] == b[k:30]:
+                cnt = k
+                break
+        else:
+            if b[0:k] == b[k:2*k]:
+                cnt = k
+                break
+
+        k += 1
+
+    print(f'#{i+1} {k}')
