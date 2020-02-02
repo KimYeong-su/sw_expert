1308624f68361692f5b2615645a79c9a4c5457d9
KimYeong-su <suwon0521@naver.com>
2020-02-03 08:53:34 +0900

    February 3rd

--- D3/password1225.py
+++ D3/password1225.py
@@ -4,25 +4,16 @@ for case in range(cases):
     c = int(input())
     base = list(map(int, input().split()))
 
-    i = 1
-    while True:
-        temp = base[0]
-        temp -= i
-        if temp<0:
-            break
-        base.remove(base[0])
-        base.append(temp)
-        i += 1
-
-    i = 1
     while base[7] != 0:
-        temp = base[0]
-        temp -= i
-        if temp<0:
-            temp = 0
-        base.remove(base[0])
-        base.append(temp)
-        i += 1
+        for i in range(1,6):
+            temp = base[0]
+            temp -= i
+            if temp < 0:
+                temp = 0
+            if base[7] == 0:
+                continue
+            base.remove(base[0])
+            base.append(temp)
     
     a = ' '.join(list(map(str,base)))
     print(f'#{c} {a}')
