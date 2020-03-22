5117e2be4980a82a68880f065746fb8769f06797
김영수 <suwon0521@naver.com>
2020-03-22 18:28:38 +0900

    Mar 22th

--- simulation_sw_test/cooker4012.py
+++ simulation_sw_test/cooker4012.py
@@ -1,19 +1,31 @@
+def f(n,k,s,e):
+    global minimum
+    if n==k:
+        temp = 0
+        for a in range(N):
+            for b in range(N):
+                if a!=b:
+                    if (a in C) and (b in C):
+                        temp += table[a][b]
+                    elif (a not in C) and (b not in C):
+                        temp -= table[a][b]
+        if minimum>abs(temp):
+            minimum = abs(temp)
+        return
+        
+    else:
+        for i in range(s, e-k+n+1):
+            C[n] = i
+            f(n+1,k,i+1,e)
+
 T = int(input())
 
 for tc in range(1,T+1):
     N = int(input())
     table = [list(map(int,input().split()))for _ in range(N)]
+    minimum = 20000
+    C = [0]*(N//2)
+    f(0,N//2,0,N)
+ 
 
-    case = []
-    for i in range(N):
-        for j in range(i+1,N):
-            case += [(i,j)]
-    result=20000
-    for i in range(len(case)):
-        for j in range(i+1, len(case)):
-            if case[j][0] not in case[i] and case[j][1] not in case[i]:
-                a,b = case[i]
-                c,d = case[j]
-                if abs(table[a][b]+table[b][a]-table[c][d]-table[d][c])<result:
-                    result = abs(table[a][b]+table[b][a]-table[c][d]-table[d][c])
-    print('#{} {}'.format(tc,result))
+    print('#{} {}'.format(tc,minimum))
