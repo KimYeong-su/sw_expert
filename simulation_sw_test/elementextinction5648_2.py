e41814718c2c4e8bcaded0b50f9ec54083bb035b
김영수 <suwon0521@naver.com>
2020-05-19 02:17:26 +0900

    May 19th

--- simulation_sw_test/elementextinction5648_2.py
+++ simulation_sw_test/elementextinction5648_2.py
@@ -1,34 +1,46 @@
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
+# dx = [0,0,-1,1]
+# dy = [1,-1,0,0]
 
 def check_atom(i,j):
     if info[i][0]==0:
         if info[j][0]==1:
             if element[i][0]==element[j][0] and element[i][1] < element[j][1]:
                 return 2
-        elif info[j][0] in [2,3]:
-            if element[i][1] < element[j][1]:
+        elif info[j][0]==2:
+            if element[i][1] < element[j][1] and element[i][0] < element[j][0] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
+                return 1
+        elif info[j][0]==3:
+            if element[i][1] < element[j][1] and element[i][0] > element[j][0] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
                 return 1
     elif info[i][0]==1:
         if info[j][0]==0:
             if element[i][0]==element[j][0] and element[i][1] > element[j][1]:
                 return 2
-        elif info[j][0] in [2,3]:
-            if element[i][1] < element[j][1]:
+        elif info[j][0]==2:
+            if element[i][1] > element[j][1] and element[i][0] < element[j][0] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
+                return 1
+        elif info[j][0]==3:
+            if element[i][1] > element[j][1] and element[i][0] > element[j][0] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
                 return 1
     elif info[i][0]==2:
         if info[j][0]==3:
             if element[i][1]==element[j][1] and element[i][0] > element[j][0]:
                 return 2
-        elif info[j][0] in [0,1]:
-            if element[i][0] > element[j][0]:
+        elif info[j][0]==0:
+            if element[i][0] > element[j][0] and element[i][1] > element[j][1] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
+                return 1
+        elif info[j][0]==1:
+            if element[i][0] > element[j][0] and element[i][1] < element[j][1] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
                 return 1
     else:
         if info[j][0]==2:
             if element[i][1]==element[j][1] and element[i][0] < element[j][0]:
                 return 2
-        elif info[j][0] in [0,1]:
-            if element[i][0] < element[j][0]:
+        elif info[j][0]==0:
+            if element[i][0] < element[j][0] and element[i][1] > element[j][1] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
+                return 1
+        elif info[j][0]==1:
+            if element[i][0] < element[j][0] and element[i][1] < element[j][1] and abs(element[i][1]-element[j][1])==abs(element[i][0]-element[j][0]):
                 return 1
     return 0
 
@@ -56,44 +68,44 @@ for tc in range(1,T+1):
                     if element[i] not in temp_e:
                         temp_e.append(element[i])
                         temp_i.append(info[i])
-                        temp_l.append(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))
+                        temp_l.append((abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2)
+                    else:
+                        idx = temp_e.index(element[i])
+                        if temp_l[idx]>(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2:
+                            temp_l[idx]=(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2
                     if element[j] not in temp_e:
                         temp_e.append(element[j])
                         temp_i.append(info[j])
-                        temp_l.append(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))
-                    if element[i] in temp_e:
-                        idx = temp_e.index(element[i])
-                        if temp_l[idx]>abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]):
-                            temp_l[idx]=abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i])
-                    if element[j] in temp_e:
+                        temp_l.append((abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2)
+                    else:
                         idx = temp_e.index(element[j])
-                        if temp_l[idx]>abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]):
-                            temp_l[idx]=abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i])
+                        if temp_l[idx]>(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2:
+                            temp_l[idx]=(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2
                 elif flag==1:
                     if element[i] not in temp_e:
                         temp_e.append(element[i])
                         temp_i.append(info[i])
-                        temp_l.append((abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))//2)
+                        temp_l.append((abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2)
+                    else:
+                        idx = temp_e.index(element[i])
+                        if temp_l[idx]>(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2:
+                            temp_l[idx]=(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2
                     if element[j] not in temp_e:
                         temp_e.append(element[j])
                         temp_i.append(info[j])
-                        temp_l.append((abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))//2)
-                    if element[i] in temp_e:
-                        idx = temp_e.index(element[i])
-                        if temp_l[idx]>(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))//2:
-                            temp_l[idx]=(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))//2
-                    if element[j] in temp_e:
+                        temp_l.append((abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2)
+                    else:
                         idx = temp_e.index(element[j])
-                        if temp_l[idx]>(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))//2:
-                            temp_l[idx]=(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][i]))//2
+                        if temp_l[idx]>(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2:
+                            temp_l[idx]=(abs(element[i][0]-element[j][0])+abs(element[i][1]-element[j][1]))//2
+        if len(temp_l)==0:
+            break
         extinction = min(temp_l)
         for i in range(len(temp_l)-1,-1,-1):
             if temp_l[i]==extinction:
+                idx = element.index(temp_e[i])
                 result+=temp_i[i][1]
-                temp_e.pop(i)
-                temp_i.pop(i)
-                temp_l.pop(i)
-        element = temp_e
-        info = temp_i
+                element.pop(idx)
+                info.pop(idx)
 
     print('#{} {}'.format(tc,result))
