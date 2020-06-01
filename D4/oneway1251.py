2e5dd55a87e2e3b190e97f61f4c26c5a1cf11819
김영수 <suwon0521@naver.com>
2020-06-01 22:56:18 +0900

    June 1st

--- D4/oneway1251.py
+++ D4/oneway1251.py
@@ -1,18 +1,24 @@
+import heapq
+
 T = int(input())
 for tc in range(1,T+1):
     N = int(input())
     x = list(map(int,input().split()))
     y = list(map(int,input().split()))
+    E = float(input())
     visit = [False]*N
-    visit[0] = True
-    for i in range(N-1):
-        temp = float('inf')
-        idx = -1
-        for j in range(i+1,N):
-            if visit[j]!=True:
-                l = (abs(x[i]-x[j])**2+abs(y[i]-y[j])**2)**0.5
-                if l<temp:
-                    temp=l
-                    idx = j
-        visit[idx]=True
-        
+    key = [float('inf')]*N
+    key[0]=0
+    pq = []
+    heapq.heappush(pq,(0,0))
+    while pq:
+        k,node = heapq.heappop(pq)
+        if visit[node]: continue
+        visit[node]=True
+        for i in range(N):
+            tax = E*(abs(x[node]-x[i])**2+abs(y[node]-y[i])**2)
+            if not visit[i] and tax<key[i]:
+                key[i]  = tax
+                heapq.heappush(pq,(key[i],i))
+    result = round(sum(key))
+    print('#{} {}'.format(tc,result))
